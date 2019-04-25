f9115015ddf56586af4cc055f748d52d916c95d1
Marco A <mabo.marcoantonio@gmail.com>
2019-04-25 11:23:13 -0300

    Commit Workspace

---
+++ NAC1_Q2/NAC1_Q2.ino
@@ -0,0 +1,48 @@
+#include "DHT.h"
+#define DHTPIN A2 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE); //Instanciação do objeto do sensor
+
+const int RED = 3;
+const int GREEN = 5;
+const int BLUE = 9; 
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+void loop() {
+  // A leitura da temperatura e umidade pode levar 250ms!
+  float h = dht.readHumidity();//Valor da umidade
+  float t = dht.readTemperature(); //Valor da temperatura
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Erro ao ler do DHT");
+  } else {
+    Serial.print("Umidade: ");
+    Serial.print(h); Serial.print(" %\t");
+    Serial.print("Temperatura: ");
+    Serial.print(t); Serial.println(" ºC");
+  }
+  if(t <= 23 && h < 40) {
+    analogWrite(RED, 0);
+    analogWrite(GREEN, 0);
+    analogWrite(BLUE, 0);
+  } else if(t > 23 && h < 40) {
+    analogWrite(RED, 0);
+    analogWrite(BLUE, 127);
+    analogWrite(GREEN, 255);
+  } else if(t <= 23 && h > 40) {
+    analogWrite(RED, 255);
+    analogWrite(BLUE, 0);
+    analogWrite(GREEN, 255);
+  } else {
+    analogWrite(RED, 0);
+    analogWrite(BLUE, 255);
+    analogWrite(GREEN, 255); 
+  }
+  delay(200);
+}
+
---
+++ NAC1_Q3/NAC1_Q3.ino
@@ -0,0 +1,49 @@
+#include <ArduinoJson.h>
+const int RED = 3;
+const int GREEN = 5;
+const int BLUE = 9;
+
+const int TAMANHO = 200;
+
+#include "DHT.h"
+#define DHTPIN A2 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE); //Instanciação do objeto do sensor
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(10); //1000ms é muito tempo
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  dht.begin();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    //Lê o texto disponível na porta serial:
+    char texto[TAMANHO];
+    Serial.readBytesUntil('\n', texto, TAMANHO);
+    //Grava o texto recebido como JSON
+    StaticJsonBuffer<TAMANHO> jsonBuffer;
+    JsonObject& json = jsonBuffer.parseObject(texto);
+    if (json.success() && json.containsKey("red")) {
+      analogWrite(RED, 255 - (int)json["red"]);
+      Serial.println(String("Vermelho: ") + (255-(int)json["red"]));
+    }
+    if (json.success() && json.containsKey("green")) {
+      analogWrite(GREEN, 255-(int)json["green"]);
+      Serial.println(String("Verde: ") + (255-(int)json["green"]));
+    }
+    if (json.success() && json.containsKey("blue")) {
+      analogWrite(BLUE, 255-(int)json["blue"]);
+      Serial.println(String("Azul: ") + (255-(int)json["blue"]));
+    }
+  }
+  StaticJsonBuffer<TAMANHO> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["temp"] = dht.readTemperature();
+  json["umid"] = dht.readHumidity();
+  json.printTo(Serial); Serial.println();
+  delay(1000);
+}
